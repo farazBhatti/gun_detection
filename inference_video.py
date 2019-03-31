2a36cb8a9cb3c8e27646a6597f148b88d022c4c5
Faraz Ahmad <37732181+farazBhatti@users.noreply.github.com>
2019-03-31 13:20:18 +0500

    Add files via upload

--- inference_video.py
+++ inference_video.py
@@ -32,7 +32,7 @@ from utils import visualization_utils as vis_util
 
 # Name of the directory containing the object detection module we're using
 MODEL_NAME = 'inference_graph'
-VIDEO_NAME = './test_videos/arnold_gun_fight.mp4'
+VIDEO_NAME = './'
 
 # Grab path to current working directory
 CWD_PATH = os.getcwd()
